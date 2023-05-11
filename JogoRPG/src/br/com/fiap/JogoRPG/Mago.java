a808e550d8eee56b46d8d881428c7012b7c5f0c2
adrian-siv <adrian.ssiv@gmail.com>
2023-05-11 09:17:48 -0300

    feat: create class "Mago"

---
+++ JogoRPG/src/br/com/fiap/JogoRPG/Mago.java
@@ -0,0 +1,16 @@
+package br.com.fiap.JogoRPG;
+
+public class Mago extends Jogador {
+
+	private int mp;
+
+	public Mago() {
+
+	}
+
+	public Mago(String nome, int mp) {
+		super(nome);
+		this.mp = mp;
+	}
+
+}
